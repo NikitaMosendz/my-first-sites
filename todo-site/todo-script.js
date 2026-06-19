c38e4637591fada5130c3b59b0ce60df780ef89b
NikitaMosendz <nikitaworkmosendz@gmail.com>
2026-06-19 10:21:23 +0300

    Fifteenth commit: feat: wraw fetch in loadTodos function and implement dynamic task creation via POST

--- todo-site/todo-script.js
+++ todo-site/todo-script.js
@@ -1,6 +1,7 @@
 const serverButton = document.getElementById('server-button');
+const addButton = document.getElementById('add-button');
 const statusTextField = document.getElementById('status-text-field');
-const textField = document.getElementById('text-field');
+const textInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 const body = document.body;
 
@@ -12,25 +13,51 @@ serverButton.addEventListener('click', () => {
     });
 });
 
+addButton.addEventListener('click', () => {
+    const taskText = textInput.value;
+
+    if (taskText === '') return;
+
+    fetch('http://localhost:4000/todos', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ title: taskText })
+    })
+    .then(response => response.json())
+    .then(newTodo => {
+        console.log("Successfully added:", newTodo);
+
+        loadTodos();
+
+        textInput.value = '';
+    })
+});
+
 fetch('http://localhost:4000/color')
     .then(response => response.json())
     .then(data => {
         body.style.backgroundColor = data.recommendedColor;
     });
 
-fetch('http://localhost:4000/todos')
-    .then(response => response.json())
-    .then(data => {
-        todoList.innerHTML = '';
+function loadTodos() {
+    fetch('http://localhost:4000/todos')
+        .then(response => response.json())
+        .then(data => {
+            todoList.innerHTML = '';
 
-        data.forEach(todo => {
-            const li = document.createElement("li");
-            li.textContent = todo.title;
+            data.forEach(todo => {
+                const li = document.createElement("li");
+                li.textContent = todo.title;
 
-            if (todo.completed === true) {
-                li.style.textDecoration = "line-through";
-            }
+                if (todo.completed === true) {
+                    li.style.textDecoration = "line-through";
+                }
 
-            todoList.appendChild(li);
+                todoList.appendChild(li);
+            });
         });
-    });
+}
+
+loadTodos();
